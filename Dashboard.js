7d318f77061e0aa3e36a19f67fa359df162d0f15
sparshmaheshwari11 <124358561+sparshmaheshwari11@users.noreply.github.com>
2024-07-01 12:21:55 +0530

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,11 @@
+// src/pages/Dashboard.js
+import React from 'react';
+
+const Dashboard = () => (
+  <div>
+    <h2>Dashboard</h2>
+    <p>Welcome to the Admin Dashboard!</p>
+  </div>
+);
+
+export default Dashboard;
