19a50747b2564e6ffcf98a5497a0bd1a8d2d653c
sparshmaheshwari11 <124358561+sparshmaheshwari11@users.noreply.github.com>
2024-07-01 12:21:05 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,13 @@
+// src/components/Footer.js
+import React from 'react';
+import { Typography, Container } from '@material-ui/core';
+
+const Footer = () => (
+  <Container>
+    <Typography variant="body1" align="center">
+      © 2024 Admin Dashboard
+    </Typography>
+  </Container>
+);
+
+export default Footer;
